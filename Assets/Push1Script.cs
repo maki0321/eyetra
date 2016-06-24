a856b91ec455b717b610f30c028fc94f2ec29ffb
菅野真生 <kannomaki@kannomasao-no-MacBook-Air.local>
2016-06-25 02:16:05 +0900

    テキストラベル復活、背景なし

--- Assets/Push1Script.cs
+++ Assets/Push1Script.cs
@@ -7,6 +7,6 @@ public class Push1Script : MonoBehaviour {
 
 	public void OnGUI () {
 		// ラベルを表示する
-		GUI.Label(new Rect(20, 20, 100, 50), "OK");
+		GUI.Label(new Rect(20, 20, 100, 50), "正解");
 	}
 }
