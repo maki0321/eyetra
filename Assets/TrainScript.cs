8a3a68e48fba01c6ba8d17b5f2718a9fb2efef4f
菅野真生 <kannomaki@kannomasao-no-MacBook-Air.local>
2016-06-25 10:52:54 +0900

    ランダム

--- Assets/TrainScript.cs
+++ Assets/TrainScript.cs
@@ -3,15 +3,18 @@ using System.Collections;
 using Random = UnityEngine.Random;
 
 public class TrainScript : MonoBehaviour {
-	public GameObject[] prefabs;
+	
+	public GameObject[] Train;
 
-
-
-
+	void Start(){
+	Instantiate(Train[Random.Range(0,Train.Length)],transform.position,transform.rotation);
+	/*
 	public static T Random<T>( params T[] values )
 	{
 		return values[ UnityEngine.Random.Range( 0, values.Length ) ];
 	}
+*/
 	
 }
 
+}
