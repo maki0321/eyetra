11b4e8d38da94e8e088cc51d462a922361fb3436
菅野真生 <kannomaki@kannomasao-no-MacBook-Air.local>
2016-06-27 22:23:29 +0900

    正解不正解

--- Assets/Scripts/TrainScript.cs
+++ Assets/Scripts/TrainScript.cs
@@ -1,12 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
 using Random = UnityEngine.Random;
+using UnityEngine.UI;
 
 public class TrainScript : MonoBehaviour
 {
 	
 	public GameObject[] Train;
 	public int number;
+	public Text messagetext;
 
 	void Start ()
 	{
@@ -28,10 +30,15 @@ public class TrainScript : MonoBehaviour
 
 	public void button (int num)
 	{
+		 
 		if (number == num) {
 			Debug.Log ("正解");
+			messagetext.text="正解";
+
+
 		} else {
 			Debug.Log ("不正解");
+			messagetext.text="不正解";
 		}
 	}
 }
