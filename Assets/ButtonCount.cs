654cca666e8acf60335133d4e541919cc3ea32ce
菅野真生 <kannomaki@kannomasao-no-MacBook-Air.local>
2016-06-27 21:14:42 +0900

    ボタンと車両の関連

--- Assets/ButtonCount.cs
+++ Assets/ButtonCount.cs
@@ -3,16 +3,20 @@ using System.Collections;
 
 public class ButtonCount : MonoBehaviour {
 
-
+	public GameObject gb;
 	public int buttonNumber;
 
 	public void Answer () {
-		Debug.Log (buttonNumber);
-		TrainScript number = GetComponent<TrainScript> ();
-		if (buttonNumber == number){
-			GUI.Label(new Rect(20, 20, 100, 50), "正解");
+		//Debug.Log (buttonNumber);
+		Debug.Log (gb.GetComponent<TrainScript> ().number);
+//		TrainScript num = GetComponent<TrainScript> ();
+		if (buttonNumber == gb.GetComponent<TrainScript> ().number) {
+			Debug.Log ("正解");
+		} else {
+			Debug.Log ("不正解");
+		}
 			
-	}
+	
 }
 }
 
--- Assets/TrainScript.cs
+++ Assets/TrainScript.cs
@@ -2,23 +2,36 @@
 using System.Collections;
 using Random = UnityEngine.Random;
 
-public class TrainScript : MonoBehaviour {
+public class TrainScript : MonoBehaviour
+{
 	
 	public GameObject[] Train;
+	public int number;
+
+	void Start ()
+	{
+		number = Random.Range (0, Train.Length);
+		Instantiate (Train [number], transform.position, transform.rotation);
 
 
-	void Start(){
-		//int number = Train [Random.Range (0, Train.Length)];
-	Instantiate(/*number*/Train[Random.Range(0,Train.Length)],transform.position,transform.rotation);
-		int number = Train.Length;
-
-	/*
+		/*
 	public static T Random<T>( params T[] values )
 	{
 		return values[ UnityEngine.Random.Range( 0, values.Length ) ];
 	}
 */
 	
-}
 
+
+	}
+
+
+	public void button (int num)
+	{
+		if (number == num) {
+			Debug.Log ("正解");
+		} else {
+			Debug.Log ("不正解");
+		}
+	}
 }
